dc7c4c900ffb30574402f47fa818dc573dc496cf
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-08-02 10:59:13 +0700

    update train_word2vec

--- scripts/train_word2vec.py
+++ scripts/train_word2vec.py
@@ -17,16 +17,22 @@ for f in list_file:
     with open(os.path.join(root_dir,f),"r",encoding="utf-8") as lines:
         for line in lines:
             row = json.loads(line.strip())
-            text = row["question"]
-            text = process(text)
-            texts.append(text)
+            try:
+                text = row["question"]
+                text = process(text)
+                texts.append(text)
+                text = row["answer"]
+                text = process(text)
+                texts.append(text)
+            except:
+                continue
 print('Load data finshed')
 print("num_text : ",len(texts))
-# s_time = time.time()
-# model = Word2Vec(texts,min_count=2,size=300)
-# model.wv.save_word2vec_format('word_vector.bin',binary=True)
-# print("Finished !")
-# print("Time train /s : ",time.time()-s_time)
+s_time = time.time()
+model = Word2Vec(texts,min_count=2,size=300)
+model.wv.save_word2vec_format('word_vector.bin',binary=True)
+print("Finished !")
+print("Time train /s : ",time.time()-s_time)
 model_word2vec = gensim.models.KeyedVectors.load_word2vec_format("word_vector.bin",binary=True)
 vocab = model_word2vec.vocab.keys()
 print(len(vocab))
